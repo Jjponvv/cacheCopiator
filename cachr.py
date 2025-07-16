fe051719e350d2dcf609686292df07c106f883b4
Jjponvv <limbogeom@gmail.com>
2025-07-16 12:00:48 +0300

    first commit

---
+++ cachr.py
@@ -0,0 +1,55 @@
+import argparse
+import json
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from urllib.parse import urlparse
+from selenium.webdriver.chrome.service import Service
+
+def save(url: str, output_file: str):
+    service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--remote-debugging-port=9222")
+    options.binary_location = r"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+
+
+    driver = webdriver.Chrome(service=service, options=options)
+
+    driver.get(url)
+    print(f"Opened: {url}")
+    print("Cache is recording... for stop press Enter")
+    input()
+
+    current_url = driver.current_url
+    cookies = driver.get_cookies()
+    local_storage = driver.execute_script("return JSON.stringify(localStorage);")
+    session_storage = driver.execute_script("return JSON.stringify(sessionStorage);")
+
+    session_data = {
+        "url": current_url,
+        "cookies": cookies,
+        "localStorage": json.loads(local_storage),
+        "sessionStorage": json.loads(session_storage)
+    }
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(session_data, f, indent=2, ensure_ascii=False)
+
+        print(f"Cache will saved {output_file}")
+    driver.quit()
+
+
+parser = argparse.ArgumentParser(description="Session recorder (CLI)")
+parser.add_argument("--url", type=str, required=True, help="URL for recording session")
+parser.add_argument("--out", type=str, help="output file")
+args = parser.parse_args()
+
+if not args.out:
+    domain = urlparse(args.url).netloc.replace('.', '_')
+    args.out = f"session_{domain}.json"
+
+save(args.url, args.out)
---
+++ session_psher.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import json
+from urllib.parse import urlparse
+import argparse
+
+options = webdriver.ChromeOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-gpu")
+options.add_argument("--disable-dev-shm-usage")
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+def restore_session(driver, session_file: str, target_url: str):
+    with open(session_file, encoding='utf-8') as f:
+        session_data = json.load(f)
+
+    parsed_url = urlparse(target_url)
+    base_url = f"{parsed_url.scheme}://{parsed_url.netloc}/"
+    driver.get(base_url)
+
+    for cookie in session_data["cookies"]:
+        if 'sameSite' in cookie and cookie['sameSite'] is None:
+            del cookie['sameSite']
+        if 'expiry' in cookie and not isinstance(cookie['expiry'], int):
+            del cookie['expiry']
+        if 'domain' in cookie and cookie['domain'].startswith('.'):
+            cookie['domain'] = cookie['domain'][1:]
+        try:
+            driver.add_cookie(cookie)
+        except Exception as e:
+            print(f"Помилка додавання cookie: {cookie.get('name')} - {e}")
+
+    driver.execute_script("""
+        let data = arguments[0];
+        for (let key in data) {
+            localStorage.setItem(key, data[key]);
+        }
+    """, session_data["localStorage"])
+
+    driver.execute_script("""
+        let data = arguments[0];
+        for (let key in data) {
+            sessionStorage.setItem(key, data[key]);
+        }
+    """, session_data["sessionStorage"])
+
+    driver.get(target_url)
+
+parser = argparse.ArgumentParser(description="session pusher")
+parser.add_argument("--url", "-u", type=str, required=True, help="URL to push all cached data")
+parser.add_argument("--cach", "-c", required=True, type=str, help="Path to cach (.json)")
+args = parser.parse_args()
+
+restore_session(driver, args.url, args.cach)
+
+input("Session pushed, press Enter to exit...")
+driver.quit()
